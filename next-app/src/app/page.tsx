6d2364cec44e88ea33296f3f500131a4605a5d52
Hoshino Hitoshi <hoshino.hitoshi@sidera.jp>
2024-01-16 02:56:04 +0000

    edit SVG sample

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image
-        src="/sample.svg"
+        src="/sample2.svg"
         alt="SVG sample"
         width={300}
         height={300}
