da6b62b5b0371baf2802b1d4d8bf6bd064f4c296
Hoshino Hitoshi <hoshino.hitoshi@sidera.jp>
2024-01-17 07:37:50 +0000

    leaning smaple for SVG

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -2,14 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src="/sample2.svg"
-        alt="SVG sample"
-        width={300}
-        height={300}
-        priority
-      />
-    </main>
+    <Image
+      src="/test.svg"
+      alt="SVG sample"
+      width={800}
+      height={200}
+      priority
+    />
   )
 }
