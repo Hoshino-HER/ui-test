8f43917a05ad9c26fe74e5ba975549866c674802
Hoshino Hitoshi <hoshino.hitoshi@sidera.jp>
2024-01-18 02:55:50 +0000

    svg circle as d3 graph

--- next-app/src/app/d3_graph.tsx
+++ next-app/src/app/d3_graph.tsx
@@ -4,58 +4,56 @@ import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
 interface IProps {
-    data?: number[];
+  data?: number[];
 }
 
 type D3Selection = d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
 
-function barGraph(selection: D3Selection, data: number[]) {
-    // const dataset = [5, 25, 45, 65, 85];
-    const dataset = data;
-    // const div = d3.create('div')
-    selection.style('background-color', '#eef')
-      .style('padding', '1em')
+function barGraph(root: D3Selection, data: number[]) {
 
-      selection.selectAll("div")
-      .data(dataset)
-      .join("div")
-      .text(d => d)
-      // .style("background-color", (d) => d <= 30 ? 'red' : 'blue')
-      .style("background-color", (d, i) => i % 2 ? 'blue' : 'orange')
-      .style('margin-bottom', '1px')
-      .style('width', d => `${d * 5}px`)
-      .style('color', 'white')
-      .style('font-family', 'sans-serif')
-      .style('text-align', 'right')
-      .style('padding-right', '0.5em')
-    return selection.node();
-  }
+  const svg = root.append("svg")
+    .attr("width", 50)
+    .attr("height", 50)
+    .attr("viewBox", [0, 0, 50, 50]);
+
+  svg
+    .append("circle")
+    .attr("cx", 25)
+    .attr("cy", 25)
+    .attr("r", 20)
+    .attr("fill", "yellow")
+    .attr("stroke", "orange")
+    .attr("stroke-width", 2);
+  return svg.node();
+}
 
 /* Component */
 export const MyD3Graph = (props: IProps) => {
-    const d3Container = useRef(null);
+  const d3Container = useRef(null);
 
-    useEffect(
-        () => {
-            if (props.data && d3Container.current) {
-                const selection = d3.select(d3Container.current);
-                barGraph(selection, props.data);
-            }
-        },
+  useEffect(
+    () => {
+      if (props.data && d3Container.current) {
+        const root = d3.select(d3Container.current);
+        barGraph(root, props.data);
+      }
+    },
 
-        /*
-            useEffect has a dependency array (below). It's a list of dependency
-            variables for this useEffect block. The block will run after mount
-            and whenever any of these variables change. We still have to check
-            if the variables are valid, but we do not have to compare old props
-            to next props to decide whether to rerender.
-        */
-        [props.data, d3Container.current])
+    /*
+        useEffect has a dependency array (below). It's a list of dependency
+        variables for this useEffect block. The block will run after mount
+        and whenever any of these variables change. We still have to check
+        if the variables are valid, but we do not have to compare old props
+        to next props to decide whether to rerender.
+    */
+    [props.data, d3Container.current])
 
-    return (
-        <div
-            className="d3-component"
-            ref={d3Container}
-        />
-    );
+  return (
+    <svg
+      className="d3-component"
+      width={400}
+      height={200}
+      ref={d3Container}
+    />
+  );
 }
