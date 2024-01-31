054f884e9f2ccf91d300c04ba36fb7789afa84ff
Hoshino Hitoshi <hoshino.hitoshi@sidera.jp>
2024-01-31 09:05:00 +0000

    implement Zoom component

--- next-app/src/components/VisxSample.tsx
+++ next-app/src/components/VisxSample.tsx
@@ -8,16 +8,20 @@ import { LinePath, Line } from '@visx/shape';
 import { curveLinear } from '@visx/curve';
 import { ParentSize } from '@visx/responsive';
 import { GridColumns, GridRows } from '@visx/grid';
+import { Zoom } from '@visx/zoom';
+// import { RectClipPath } from '@visx/clip-path';
+import { ProvidedZoom } from '@visx/zoom/lib/types';
 import { getDeviceDrawLine } from '@/utils/drawSequenceDiagram';
 
 
-interface IPropsVisxSample extends React.SVGProps<SVGSVGElement> {
+export interface IPropsVisxSample extends React.SVGProps<SVGSVGElement> {
   story?: Story;
 }
 
 interface IPropsVisxSampleClient extends IPropsVisxSample {
   width: number;
   height: number;
+  zoom: ProvidedZoom<SVGSVGElement>;
 }
 
 const yScale = scaleBand({
@@ -73,54 +77,84 @@ const gridRowsProps = {
   scale: yScale
 }
 
+const initialTransform = {
+  scaleX: 1.27,
+  scaleY: 1.27,
+  translateX: 12,
+  translateY: -13,
+  skewX: 0,
+  skewY: 0,
+};
 
 export default function VisxSample(props: IPropsVisxSample) {
   return <ParentSize>
-    {({ width, height }) => <VisxSampleClient {...props} width={width} height={height} />}
+    {({ width, height }) => {
+      return (<div className='absolute'>
+        <Zoom<SVGSVGElement>
+          width={width} height={height}
+          scaleXMin={1} scaleXMax={2} scaleYMin={1} scaleYMax={10}
+          initialTransformMatrix={initialTransform}
+        >
+          {zoom => {
+            return (<>
+              {/* <RectClipPath id="zoom-clip" width={width} height={height} /> */}
+              <svg ref={zoom.containerRef}>
+                <g transform={zoom.toString()}>
+                  <VisxSampleClient
+                    {...props} width={width} height={height} zoom={zoom}
+                  />
+                </g>
+              </svg>
+              {`zoom: ${zoom.toString()}`}
+            </>)
+          }}
+        </Zoom>
+      </div>)
+    }}
   </ParentSize>
 }
 
 function VisxSampleClient(props: IPropsVisxSampleClient) {
-  const explain = JSON.stringify(props, null, 2).split('\n');
   const lineData = props.story ? getDeviceDrawLine<boolean>(props.story) : [];
-  const drawData = lineData.map(d => { return { x: d.time, y: yScale(d.val.toString()) } });
 
-  return <svg>
-    <GridColumns
-      width={props.width} height={props.height}
-      {...gridColumnProps}
-    />
-    <GridRows
-      width={props.width} height={props.height}
-      {...gridRowsProps}
-    />
-    <AxisLeft {...axisLeftProps} />
-    <Axis {...axisBottomProps} />
-    <LinePath
-      data={lineData}
-      // data={[{x: 0, y: 50}, {x: 400, y: 50}, {x: 400, y: 100}, {x: 0, y: 100}]}
-      x={d => xScale(d.time) ?? 0}
-      y={d => yScale(d.val.toString()) ?? 0}
-      curve={curveLinear}
-      fill="transparent" stroke="red" strokeWidth="2"
-    />
-    {lineData.map((line, i) =>
+  return (
+    <g>
+      <GridColumns
+        width={props.width} height={props.height}
+        {...gridColumnProps}
+      />
+      <GridRows
+        width={props.width} height={props.height}
+        {...gridRowsProps}
+      />
+      <AxisLeft {...axisLeftProps} />
+      <Axis {...axisBottomProps} />
+      <LinePath
+        data={lineData}
+        // data={[{x: 0, y: 50}, {x: 400, y: 50}, {x: 400, y: 100}, {x: 0, y: 100}]}
+        x={d => xScale(d.time) ?? 0}
+        y={d => yScale(d.val.toString()) ?? 0}
+        curve={curveLinear}
+        fill="transparent" stroke="red" strokeWidth="2"
+      />
+      {lineData.map((line, i) =>
+        <Text
+          fontSize={6}
+          // key={i} x={ line.search(/\S|$/) * 5} y={i * 7 + 50} 
+          key={i} x={0} y={i * 7 + 80}
+          width={300} height={100} verticalAnchor="start">
+          {`${line.time} ${line.val} ${yScale(line.val.toString())?.toFixed(1)}`}
+        </Text>
+      )}
       <Text
-        fontSize={6}
+        fontSize={20}
         // key={i} x={ line.search(/\S|$/) * 5} y={i * 7 + 50} 
-        key={i} x={0} y={i * 7 + 80}
-        width={300} height={100} verticalAnchor="start">
-        {`${line.time} ${line.val} ${yScale(line.val.toString())?.toFixed(1)}`}
+        x="10" y="5"
+        width={200} height={100} verticalAnchor="start"
+        stroke='red' fill='red'
+      >
+        {`yScale: ${yScale.bandwidth().toFixed(1)}, ${yScale.step().toFixed(1)}`}
       </Text>
-    )}
-    <Text
-      fontSize={20}
-      // key={i} x={ line.search(/\S|$/) * 5} y={i * 7 + 50} 
-      x="10" y="5"
-      width={200} height={100} verticalAnchor="start"
-      stroke='red' fill='red'
-    >
-      {`yScale: ${yScale.bandwidth().toFixed(1)}, ${yScale.step().toFixed(1)}`}
-    </Text>
-  </svg>;
+    </g>
+  );
 }
